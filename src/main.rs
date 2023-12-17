0cf271c3f9356bd538ff126c7521f0c99dc6a85a
Peter Simonsson <peter.simonsson@gmail.com>
2023-12-17 20:13:48 +0100

    Use rustyline for prompt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 mod args;
-mod prompt;
 
 use std::io;
 
@@ -11,8 +10,9 @@ use crossterm::{
     terminal::{EnterAlternateScreen, LeaveAlternateScreen},
 };
 use datafusion::prelude::*;
+use rustyline::{error::ReadlineError, DefaultEditor};
 
-use crate::{args::Args, prompt::Prompt};
+use crate::args::Args;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -31,19 +31,29 @@ async fn main() -> Result<()> {
     let options = CsvReadOptions::new().has_header(!args.no_header());
     ctx.register_csv(table, args.file(), options).await?;
 
-    let mut prompt = Prompt::new("SQL>");
+    let mut prompt = DefaultEditor::new()?;
 
     loop {
-        let buf = prompt.prompt()?;
+        let buf = prompt.readline(">> ");
 
-        if buf.starts_with("quit") {
-            break;
+        match buf {
+            Ok(buf) => {
+                if buf.starts_with("quit") {
+                    break;
+                }
+
+                prompt.add_history_entry(&buf)?;
+
+                match ctx.sql(&buf).await {
+                    Ok(df) => df.show().await?,
+                    Err(e) => println!("{}", e),
+                }
+            }
+            Err(ReadlineError::Eof) => break,
+            Err(ReadlineError::Interrupted) => {}
+            Err(e) => println!("Error: {:?}", e),
         }
 
-        match ctx.sql(&buf).await {
-            Ok(df) => df.show().await?,
-            Err(e) => println!("{}", e),
-        }
         println!();
     }
 
